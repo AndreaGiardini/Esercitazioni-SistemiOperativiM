7e2d81b3e50137cb10129f9788ed7c10c73fe566
Alessandro Colace <dok.son@msn.com>
2013-12-03 17:43:39 +0100

    Update Es1.c

--- Esercitazione1/Es1.c
+++ Esercitazione1/Es1.c
@@ -1,15 +1,15 @@
 /*
-* Si calcoli il massimo in un insieme di valori interi 
-* di N elementi, memorizzati in un vettore V. 
-* Si vuole affidare la ricerca del massimo a un 
-* insieme di M thread concorrenti, ognuno dei quali 
-* si dovrà occupare della ricerca del massimo in una 
-* porzione del vettore di dimensione K data (M=N/
-* K). 
-* Il thread iniziale dovrà quindi: 
-* –  Inizializzare il vettore V con N valori casuali; 
-* –  Creare gli M thread concorrenti; 
-* –  Ricercare il massimo tra gli M risultati ottenuti 
+* Si calcoli il massimo in un insieme di valori interi
+* di N elementi, memorizzati in un vettore V.
+* Si vuole affidare la ricerca del massimo a un
+* insieme di M thread concorrenti, ognuno dei quali
+* si dovrà occupare della ricerca del massimo in una
+* porzione del vettore di dimensione K 
+* data (M=N/K).
+* Il thread iniziale dovrà quindi:
+* –  Inizializzare il vettore V con N valori casuali;
+* –  Creare gli M thread concorrenti;
+* –  Ricercare il massimo tra gli M risultati ottenuti
 * dai thread e stamparne il valore.
 */
 
@@ -17,82 +17,88 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define MAX_RND_NUM 1050    /* Massimo numero random */
-#define NUM_EL 100          /* Numero di elementi */
-#define NUM_TH 20           /* Numero di Thread */
+#define MAX_RND_NUM 1050 // Massimo numero random
+#define NUM_EL 100 // Numero di elementi
+#define NUM_TH 10 // Numero di Thread
 
-/*
-* Lista elementi
-*/
-int elements[NUM_EL];
-
-/*
-* Numero di elementi per thread
-*/
+// Vettore degli elementi
+int V[NUM_EL];
+// Numero di elementi che deve processare ogni thread
 int el_num[NUM_TH];
 
-void* thread_work(void* t){
-    long tid = (long) t;
+// INIZIO - lavoro di ogni singolo Thread
+void* thread_work(void* t)
+{
+    long tid = (long) t; //thread-id
     int i, displ=0;
     long result=0;
 
-    /* Displacement */
-    for(i=0; i < tid; i++){
-        displ += el_num[i];
+    // Displacement = numero di partenza dove ogni thread deve partire a esaminare il vettore di elementi V
+    // ogni thread si conta autonomamente il proprio displacement		
+    for(i=0; i < tid; i++)
+        displ += el_num[i];	
+	
+	// Trovo il massimo nella parte di vettore di competenza
+    for(i=0; i < el_num[tid]; i++)
+	{
+        if(V[displ+i] > result)
+            result = V[displ+i];
     }
 
-    for(i=0; i < el_num[tid]; i++){
-        /* printf("Thread id: %ld - Displacement: %d - Number : %d\n", tid, displ, elements[displ+i]); */
-        if(elements[displ+i] > result){
-            result = elements[displ+i];
-        }
-    }
+    pthread_exit((void*) result);
+} 
+// FINE - lavoro di ogni singolo Thread
 
-    pthread_exit((void*) result); 
-}
-
-int main (int argc, char* argv[]){
-
-    /*
-    * i - Indice
-    * rc - Esito primitive
-    * status - Codici di ritorno
-    * thread - Lista thread
-    */
+int main (int argc, char* argv[])
+{
+	// indice
     long i;
+	// esito primitive
     int rc;
+	// status - Codici di ritorno
     void* status;
+	// thread - Lista thread
     pthread_t thread[NUM_TH];
 
     printf("Numero di elementi: %d\nNumero casuale massimo: %d\nNumero di thread: %d\n", NUM_EL, MAX_RND_NUM, NUM_TH);
 
-    /* Inizializzazione del vettore e stampa */
-    printf("\nElements List:\n");
+    // Inizializzazione del vettore e stampa 
+    printf("\nVettore di elementi:\n");
     srand(time(NULL));
-    for(i=0; i < NUM_EL; i++){
-        elements[i]= rand() % MAX_RND_NUM;
-        printf("%d\t", elements[i]);
+	
+	// Creazione casuale degli elementi
+    for(i=0; i < NUM_EL; i++)
+	{
+        V[i]= rand() % MAX_RND_NUM;
+        // Ogni tanto va a capo per migliorare la leggibilità sulla shell
+		if(i%(NUM_EL / NUM_TH)==0)
+			printf("\n");
+        printf("%d\t", V[i]);
     }
     printf("\n");
 
-    /* Calcolo il numero di elementi che deve processare ogni thread */
-    for(i=0; i < NUM_TH; i++){
+	// INIZIO CALCOLO (numero di elementi che deve processare ogni thread)
+	// Assegno ad ogni thread almeno NUM_EL/NUM_TH
+    for(i=0; i < NUM_TH; i++)
         el_num[i] = NUM_EL / NUM_TH;
-    }
-
-    if (NUM_EL % NUM_TH != 0){
-        for(i=0; i < NUM_EL % NUM_TH; i++){
+	
+	// Verifico che non avanzino degli elementi orfani"
+	int resto = NUM_EL % NUM_TH;
+	// In caso il numero degli elementi diviso il numero di thread dia un resto, assegno gli elementi mancanti ai primi (RESTO) thread
+    if (resto != 0)
+        for(i=0; i < resto; i++)
             el_num[i]++;
-        }
-    }
 
-    /* Creazione Threads */
-    for(i=0; i < NUM_TH ; i++){
-        //printf("LOL - %lu\n", i);
+    // Creazione Threads (uso primitiva pthread_create)
+    for(i=0; i < NUM_TH ; i++)
+	{
         /* Passo al thread il suo indice */
-        rc = pthread_create(&thread[i], NULL, thread_work, (void*)i); 
-        if (rc) { 
-            printf("ERRORE: %d\n", rc); 
+        rc = pthread_create(&thread[i], NULL, thread_work, (void*)i);
+		
+		// In caso di errore, stampa e termina        
+		if (rc) 
+		{
+            printf("ERRORE: %d\n", rc);
             exit(-1);
         }
     }
@@ -101,21 +107,25 @@ int main (int argc, char* argv[]){
 
     /* Join e calcolo del massimo */
     long max=0;
-    for(i=0; i < NUM_TH ; i++){
-        rc = pthread_join(thread[i], &status); 
-        if (rc){
-            printf("ERRORE join thread %lu codice %d\n", i, rc); 
-        }else{
+    for(i=0; i < NUM_TH ; i++)
+	{
+        rc = pthread_join(thread[i], &status);
+        if (rc)
+		{
+            printf("ERRORE join thread %lu codice %d\n", i, rc);
+        }
+		else
+		{
             printf("Finito thread con ris. %ld\n",(long)status);
-            if((long)status > max){
+						
+			// Al termine di ogni Thread, se ha trovato un massimo più alto, consideralo il massimo
+            if((long)status > max)
                 max = (long)status;
-            }
         }
     }
 
     /* Risultato */
-    printf("\nMaximum is: %lu", max);
+    printf("\nMaximum is: %lu\n", max);
 
     return 0;
-
 }
