ff69a0ff7aebe8bef9f44496ff14bee3650f6a84
dokson <dok.son@msn.com>
2013-12-12 17:10:19 +0100

    indentazione corretta con il nuovo gedit mitico

--- Esercitazione6/tavolo.adb
+++ Esercitazione6/tavolo.adb
@@ -2,26 +2,26 @@ with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Discrete_Random;
 use Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure tavolo is
-  -- Costanti
+	-- Costanti
 	MaxC : constant integer := 20; -- torte massime tavolo
 
-  id_MIN : constant integer := 1; --id operaio minimo
-  id_MAX : constant integer := 40; --id operaio massimo
+	id_MIN : constant integer := 1; --id operaio minimo
+	id_MAX : constant integer := 40; --id operaio massimo
 
 	-- Definizioni
-  type cliente_ID is range id_MIN..id_MAX;
+	type cliente_ID is range id_MIN..id_MAX;
 	type torta is (cioccolata, marmellata);
 	type confezione is (cioccolata, marmellata, famiglia);
 
 	-- Generatore di torte casuali
 	package Torta_Random is new Ada.Numerics.Discrete_Random(torta);
-	use Torta_Random;	
-	TG: Torta_Random.Generator;	
+	use Torta_Random;
+	TG: Torta_Random.Generator;
 
 	-- Generatore di confezioni casuali
 	package Confezione_Random is new Ada.Numerics.Discrete_Random(confezione);
 	use Confezione_Random;
-	CG: Confezione_Random.Generator;	
+	CG: Confezione_Random.Generator;
 
 	-- DEFINIZIONE GESTORE DEL TAVOLO
 	task type server is 
@@ -37,44 +37,44 @@ procedure tavolo is
 		torteMarmellata: integer; --numero di torte alla marmellata sul tavolo
 	begin
 		-- Inizializzazione variabili di stato del tavolo
-    torteCioccolata :=0;
-    torteMarmellata :=0;
+		torteCioccolata :=0;
+		torteMarmellata :=0;
 		--Gestione richieste:
     loop
     	select
 				-- ACCETTAZIONE RICHIESTE DEPOSITO TORTE SULLA TAVOLA
-        -- priorità alla marmellata
-        when (torteCioccolata + torteMarmellata) < MaxC =>
-          accept deposito(marmellata)(ID: cliente_ID) do 
-            torteMarmellata:=torteMarmellata+1;
-            Put_Line("OP (id:" & cliente_ID'Image (ID) & ") ha depositato una torta alla "&torta'Image (marmellata)&" sul tavolo");
-          end deposito;
+				-- priorità alla marmellata
+				when (torteCioccolata + torteMarmellata) < MaxC =>
+					accept deposito(marmellata)(ID: cliente_ID) do
+						torteMarmellata:=torteMarmellata+1;
+						Put_Line("OP (id:" & cliente_ID'Image (ID) & ") ha depositato una torta alla "&torta'Image (marmellata)&" sul tavolo");
+					end deposito;
 				-- se non ci sono depositi di marmellata in attesa, deposita torta alla cioccolata
 				or when (torteCioccolata + torteMarmellata) < MaxC and deposito(marmellata)'COUNT = 0 =>
-          accept deposito(cioccolata)(ID: cliente_ID) do 
-            torteCioccolata:=torteCioccolata+1;
-            Put_Line("OP (id:" & cliente_ID'Image (ID) & ") ha depositato una torta alla "&torta'Image (cioccolata)&" sul tavolo");
-          end deposito;
+					accept deposito(cioccolata)(ID: cliente_ID) do
+						torteCioccolata:=torteCioccolata+1;
+						Put_Line("OP (id:" & cliente_ID'Image (ID) & ") ha depositato una torta alla "&torta'Image (cioccolata)&" sul tavolo");
+					end deposito;
 				-- ACCETTAZIONE RICHIESTE PRELIEVO TORTE DALLA TAVOLA PER CREARE LE CONFEZIONI
 				-- priorità alle confezioni famiglia
 				or when torteCioccolata > 0 and torteMarmellata > 0 =>
-					accept prelievo(famiglia)(ID: cliente_ID) do 
-            torteMarmellata:=torteMarmellata-1;
+					accept prelievo(famiglia)(ID: cliente_ID) do
+						torteMarmellata:=torteMarmellata-1;
 						torteCioccolata:=torteCioccolata-1;
-            Put_Line("OC (id:" & cliente_ID'Image (ID) & ") ha prelevato una torta per tipo per creare una confezione "&confezione'Image (famiglia));
-          end prelievo;
+						Put_Line("OC (id:" & cliente_ID'Image (ID) & ") ha prelevato una torta per tipo per creare una confezione "&confezione'Image (famiglia));
+					end prelievo;
 				-- tra le confezioni semplici, priorità alla marmellata
 				or when torteMarmellata > 0 and prelievo(famiglia)'COUNT = 0 =>
-					accept prelievo(marmellata)(ID: cliente_ID) do 
-            torteMarmellata:=torteMarmellata-1;
-            Put_Line("OC (id:" & cliente_ID'Image (ID) & ") ha prelevato una torta alla "&confezione'Image (marmellata)&" per creare una confezione");
-          end prelievo;
+					accept prelievo(marmellata)(ID: cliente_ID) do
+						torteMarmellata:=torteMarmellata-1;
+						Put_Line("OC (id:" & cliente_ID'Image (ID) & ") ha prelevato una torta alla "&confezione'Image (marmellata)&" per creare una confezione");
+					end prelievo;
 				-- se non ci sono altre richieste in sospeso, crea confezione cioccolata
 				or when torteCioccolata > 0 and prelievo(famiglia)'COUNT = 0 and prelievo(marmellata)'COUNT = 0 =>
-					accept prelievo(cioccolata)(ID: cliente_ID) do 
-            torteCioccolata:=torteCioccolata-1;
-            Put_Line("OC (id:" & cliente_ID'Image (ID) & ") ha prelevato una torta alla "&confezione'Image (cioccolata)&" per creare una confezione");
-          end prelievo;
+					accept prelievo(cioccolata)(ID: cliente_ID) do
+						torteCioccolata:=torteCioccolata-1;
+						Put_Line("OC (id:" & cliente_ID'Image (ID) & ") ha prelevato una torta alla "&confezione'Image (cioccolata)&" per creare una confezione");
+					end prelievo;
 				or terminate;
 			end select;
 		end loop;
@@ -83,23 +83,23 @@ procedure tavolo is
 	-- Dichiarazione gestore del tavolo
 	S: server; 
 
-  -- DEFINIZIONE DEI TASK CLIENTI
-  task type clienteOP (ID: cliente_ID; T: torta);
-	task type clienteOC (ID: cliente_ID; C: confezione);	
+	-- DEFINIZIONE DEI TASK CLIENTI
+	task type clienteOP (ID: cliente_ID; T: torta);
+	task type clienteOC (ID: cliente_ID; C: confezione);
 
 	-- CORPO DEI TASK CLIENTI
 	task body clienteOP is
-  begin
-    Put_Line("L'operaio OP (id:"& cliente_ID'Image (ID)&") vuole depositare una torta alla "&torta'Image (T));
-    S.deposito(T)(ID);
-  end clienteOP;
+	begin
+		Put_Line("L'operaio OP (id:"& cliente_ID'Image (ID)&") vuole depositare una torta alla "&torta'Image (T));
+		S.deposito(T)(ID);
+	end clienteOP;
 
 	task body clienteOC is
-  begin
-    Put_Line("L'operaio OC (id:"& cliente_ID'Image (ID)&") vuole prelevare una confezione "&confezione'Image (C));
-    S.prelievo(C)(ID);
-  end clienteOC;
-	
+	begin
+		Put_Line("L'operaio OC (id:"& cliente_ID'Image (ID)&") vuole prelevare una confezione "&confezione'Image (C));
+		S.prelievo(C)(ID);
+	end clienteOC;
+
 	-- Riferimenti ai task clienti
 	type OP is access clienteOP;
 	type OC is access clienteOC;
@@ -112,17 +112,17 @@ begin
 	-- Inizializza i generatori in uno stato diverso ad ogni run
 	Reset(TG);
 	Reset(CG);
-  for I in cliente_ID'range loop -- ciclo creazione task
-    -- creazione operaio I-esimo
+	for I in cliente_ID'range loop -- ciclo creazione task
+		-- creazione operaio I-esimo
 		-- gli operai dispari depositano
-		if I rem 2 = 1 
+		if I rem 2 = 1
 		then
-		  -- Genero torta casuale
-    	operaiOP(I) := new clienteOP(I, Random(TG));
+			-- Genero torta casuale
+			operaiOP(I) := new clienteOP(I, Random(TG));
 		-- gli operai pari prelevano
 		else
 			-- Genero confezione casuale
-    	operaiOC(I) := new clienteOC(I, Random(CG)); 
+			operaiOC(I) := new clienteOC(I, Random(CG)); 
 		end if;
-  end loop;
+	end loop;
 end tavolo;
